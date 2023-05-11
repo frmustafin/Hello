7b92d4509dc7b3e4c731d3e0233b88cfc6d6b189
Farid <frmustafin@domclick.ru>
2023-05-11 22:58:53 +0300

    Revert "Initial commit"
    This reverts commit 2f63b2d248b1f55718df61088050d35d46dcb832.

--- m1l1-hello/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
-plugins {
-    kotlin("jvm") 
-}
-
-dependencies {
-    testImplementation(kotlin("test-junit"))
-}
--- m1l1-hello/src/main/kotlin/Main.kt
+++
@@ -1,6 +0,0 @@
-
-fun main(args: Array<String>) {
-    println("Hello world")
-
-    println("Program args: ${args.joinToString()}")
-}
